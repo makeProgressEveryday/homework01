b8b9d42171eb2133fd50fb62933e9db84e9e87df
刘春牢 <liuchunlao@qq.com>
2016-04-25 10:10:23 +0800

    显示自定义键盘
    1.懒加载文本框
    2.懒加载工具条
    3.懒加载pickerView
    4.点击按钮时
    ——将文本框添加到label上
    ——让文本框成为第一响应者

--- homework01/ViewController.m
+++ homework01/ViewController.m
@@ -11,7 +11,16 @@
 @interface ViewController ()
 
 // 显示数据的label
-@property (nonatomic, weak) UILabel *label;
+@property (nonatomic, weak) IBOutlet UILabel *label;
+
+
+// 日期选择控件
+@property (nonatomic, strong) UIPickerView *pickerView;
+
+// 工具条
+@property (nonatomic, strong) UIToolbar *toolbar;
+
+@property (nonatomic, strong) UITextField *txtField;
 
 @end
 
@@ -27,16 +36,83 @@
  */
 
 - (void)viewDidLoad {
+
     [super viewDidLoad];
-    
+
 }
 
 
 - (IBAction)btnClick:(id)sender {
 
+    // 通过文本框调出键盘
+    [self.label addSubview:self.txtField];
+    [self.txtField becomeFirstResponder];
     
+}
+
+#pragma mark - 取消，确认按钮的点击事件
+- (void)cancelItemClick {
     
+    [self.view endEditing:YES];
+}
+
+- (void)confirmItemClick {
     
+   
+    
+}
+
+
+#pragma mark - 懒加载
+- (UITextField *)txtField {
+
+    if (_txtField == nil) {
+        
+        _txtField = [[UITextField alloc] init];
+        
+        _txtField.inputView = self.pickerView;
+        _txtField.inputAccessoryView = self.toolbar;
+    }
+    return _txtField;
+
+}
+
+- (UIPickerView *)pickerView {
+
+    if (_pickerView == nil) {
+        _pickerView = [[UIPickerView alloc] init];
+        
+        
+    }
+    return _pickerView;
+    
+}
+
+
+- (UIToolbar *)toolbar {
+    if (!_toolbar) {
+        
+        // 创建工具条
+        _toolbar = [[UIToolbar alloc] init];
+        // 设置高度就可以了，剩下的系统会自动处理
+        _toolbar.frame = CGRectMake(0, 0, 0, 44);
+        
+        // 添加工具条里面的按钮
+        // 取消
+        UIBarButtonItem *cancelItem = [[UIBarButtonItem alloc] initWithTitle:@"取消" style:UIBarButtonItemStylePlain target:self action:@selector(cancelItemClick)];
+        
+        // 弹簧
+        UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+        
+        // 确认
+        UIBarButtonItem *confirmItem = [[UIBarButtonItem alloc] initWithTitle:@"确认" style:UIBarButtonItemStylePlain target:self action:@selector(confirmItemClick)];
+        
+        // 将这三个按钮添加到工具条内
+        _toolbar.items = @[cancelItem, flexSpace, confirmItem];
+        
+        
+    }
+    return _toolbar;
 }
 
 
